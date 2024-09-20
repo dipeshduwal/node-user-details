e688ee871eca7fd998f72ae39d8edf85702ab026
dipeshduwal <electrifyingdipesh1@gmail.com>
2024-09-20 14:27:58 +0545

    Create router file

--- app.js
+++ app.js
@@ -5,3 +5,6 @@ const usersRouter = require("./routes/usersRouter");
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true}));
 app.use("/", usersRouter);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Express app listening on port ${PORT}!`));
