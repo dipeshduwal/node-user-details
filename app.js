116ce1b5fb559a90f88c53052117f7f0159badfc
dipeshduwal <electrifyingdipesh1@gmail.com>
2024-09-20 14:14:36 +0545

    Set up express project files and working directories

---
+++ app.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const app = express();
+const usersRouter = require("./routes/usersRouter");
+
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true}));
+app.use("/", usersRouter);
